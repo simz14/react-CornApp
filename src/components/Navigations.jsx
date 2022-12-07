04f43be82670cdbc1672a9094758754c46118e20
Simona Zozuľaková <szozulakova@gmail.com>
2022-12-07 22:30:39 +0100

    styling done

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const NavList = styled.ul`
+  margin: 0;
+  padding: 25px;
+`;
+const Link = styled(NavLink)`
+  background-color: #0000004a;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 10px;
+  text-decoration: none;
+  color: white;
+  &:hover {
+    background-color: #000000bd;
+  }
+`;
 
 const Navigation = () => {
   return (
     <div>
-      <ul>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/simple/states">Counter</NavLink>
-      </ul>
+      <NavList>
+        <Link to="/">Home</Link>
+        <Link to="/simple/states">Counter</Link>
+      </NavList>
     </div>
   );
 };
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+import cornImage from "../assets/corn.gif";
+
+const HomeWrapper = styled.div`
+  display: grid;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  display: flex;
+  justify-content: center;
+`;
+
+const CornImage = styled.img`
+  width: 650px;
+`;
 
 const Home = () => {
   return (
-    <div>
-      <h1>Golden Corn is life 🌽</h1>
-    </div>
+    <HomeWrapper>
+      <Title>Golden Corn is life 🌽</Title>
+      <CornImage src={cornImage} alt="Corn giph" />
+    </HomeWrapper>
   );
 };
 
